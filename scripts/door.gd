25c18041c0489d37d116ab2ce63560fa6b5836a5
hcyk268 <vvhipt206.contact@gmail.com>
2026-04-16 19:11:20 +0700

    feat: spike, button, key, and door

---
+++ scripts/door.gd
@@ -0,0 +1,50 @@
+extends StaticBody2D
+
+signal door_opened
+signal door_closed
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var detection_area: Area2D = $DetectionArea
+
+var is_open := false
+
+
+func _ready() -> void:
+	detection_area.body_entered.connect(_on_detection_area_body_entered)
+	_apply_state()
+
+
+func set_open(value: bool) -> void:
+	if is_open == value:
+		return
+
+	is_open = value
+	_apply_state()
+
+	if is_open:
+		door_opened.emit()
+	else:
+		door_closed.emit()
+
+
+func open() -> void:
+	set_open(true)
+
+
+func close() -> void:
+	set_open(false)
+
+
+func _on_detection_area_body_entered(body: Node) -> void:
+	if is_open or not body.is_in_group("player"):
+		return
+
+	if body.has_method("has_key") and body.has_key():
+		open()
+
+
+func _apply_state() -> void:
+	animated_sprite.play("open" if is_open else "closed")
+	collision_shape.disabled = is_open
+	detection_area.monitoring = not is_open
